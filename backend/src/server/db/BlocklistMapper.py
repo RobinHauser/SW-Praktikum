26ea8d4428cbcc4b45bb27ddeac15d59b1aead60
Ben <ben@slne.dev>
2023-04-25 17:20:38 +0200

    Init of BlocklistMapper

---
+++ backend/src/server/db/BlocklistMapper.py
@@ -0,0 +1,22 @@
+import server.db.Mapper
+
+
+class BlocklistMapper(Mapper):
+
+    def __init__(self):
+        super().__init__()
+
+    def find_all(self):
+
+        result = []
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT * FROM blocklist")
+        tuples = cursor.fetchall()
+
+        for () in tuples:
+
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
