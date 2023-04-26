12de467deeceaf4f5184fc6d399b352c794a5f62
Theo Klautke <theoklautkemail@gmail.com>
2023-04-26 22:59:23 +0200

    Added SignIn Screen

---
+++ frontend/sopra-frontend/src/components/AppHeaderLight.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+
+export default function AppHeaderLight() {
+    return (
+        <AppBar position="static">
+            <Container maxWidth="xl">
+                <Toolbar disableGutters>
+                    <Typography
+                        variant="h6"
+                        noWrap
+                        component="a"
+                        sx={{
+                            display: {xs: 'none', md: 'flex'},
+                            fontWeight: 700,
+                            color: 'inherit',
+                            textDecoration: 'none',
+                        }}>
+                        Sopra Dating
+                    </Typography>
+                </Toolbar>
+            </Container>
+        </AppBar>
+    );
+}
--- frontend/sopra-frontend/src/pages/Main.js
+++ frontend/sopra-frontend/src/pages/Main.js
@@ -1,11 +1,11 @@
-import MenuAppBar from "../components/AppHeader";
+import AppHeader from "../components/AppHeader";
 import Container from "@mui/material/Container";
 import GridContainer from "../components/GridContainer";
 
 export default function Main() {
     return (
         <div className="App">
-            <MenuAppBar></MenuAppBar>
+            <AppHeader></AppHeader>
             <Container style={{marginTop: '50px'}}>
                 <GridContainer></GridContainer>
             </Container>
