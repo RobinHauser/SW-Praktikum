a41a3937ae7daefc5707516a8d91a545894a8621
Robin Hauser <rh086@hdm-stuttgart.de>
2023-04-28 13:58:50 +0200

    Added Information.py, Mapper for Message and Profile

---
+++ backend/src/server/bo/Information.py
@@ -0,0 +1,6 @@
+from server.bo import BusinessObject as bo
+
+class Information(bo.BusinessObject):
+
+    def __init__(self):
+        super().__init__()
---
+++ backend/src/server/db/ProfileMapper.py
@@ -0,0 +1,84 @@
+from server.bo import Profile
+from server.db import Mapper
+
+
+class ProfileMapper(Mapper):
+
+    def __init__(self):
+        super().__init__()
+
+    def find_all(self):
+        result = []
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT * FROM profile")
+        tuples = cursor.fetchall()
+
+        for (id) in tuples:
+            profile = Profile()
+            profile.set_id(id)
+            result.append(profile)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def find_by_id(self, id):
+        result = None
+        cursor = self._cnx.cursor()
+        command = "SELECT * FROM profile WHERE id={}".format(id)
+        cursor.execute(command)
+        tuples = cursor.fetchall()
+
+        try:
+            (id) = tuples[0]
+            profile = Profile()
+            profile.set_id(id)
+            result = profile
+        except IndexError:
+            result = None
+
+        self._cnx.commit()
+        cursor.close()
+
+        return result
+
+    def insert(self, profile):
+        cursor = self._cnx.cursor()
+        cursor.execute("SELECT MAX(id) AS maxid FROM profile")
+        tuples = cursor.fetchall()
+
+        for (maxid) in tuples:
+            profile.set_id(maxid[0] + 1)
+
+        command = "INSERT INTO profile (id) VALUES (%s)"
+        data = (profile.get_id())
+        cursor.execute(command, data)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return profile
+
+    def update(self, profile):
+        cursor = self._cnx.cursor()
+
+        command = "UPDATE profile SET id=%s WHERE id=%s"
+        data = (profile.get_id(), profile.get_id())
+        cursor.execute(command, data)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return profile
+
+    def delete(self, profile):
+        cursor = self._cnx.cursor()
+
+        command = "DELETE FROM profile WHERE id={}".format(profile.get_id())
+        cursor.execute(command)
+
+        self._cnx.commit()
+        cursor.close()
+
+        return profile
