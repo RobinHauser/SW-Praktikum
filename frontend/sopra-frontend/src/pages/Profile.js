86d94bf3dad75da3757ec0e58a4c39a1c35d2c3b
Björn Till <bjoern.till@web.de>
2023-04-29 23:41:08 +0200

    Added email, name and profile picture to profile page

--- frontend/sopra-frontend/src/pages/Profile.js
+++ frontend/sopra-frontend/src/pages/Profile.js
@@ -1,13 +1,37 @@
 import AppHeader from "../components/AppHeader";
 import Container from "@mui/material/Container";
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import placeHolderImage from "../static/images/profileImagePlaceholder.jpeg";
 
 export default function Profile() {
     return (
         <div className="App">
             <AppHeader></AppHeader>
-            <Container style={{marginTop: '50px'}}>
-                Profile
+            <Container sx={{display: 'grid', placeItems: 'center', marginTop: '50px', marginBottom: '50px', fontSize: '25px'}}>
+                Profil bearbeiten
             </Container>
+
+            <Card sx={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                <Box sx={{ display: 'flex', flexDirection: 'column', width: '40%' }}>
+                    <CardContent>
+                        <Typography component="div" variant="h6" sx={{ textAlign: 'left' }}>
+                                Name: Hans Jürgen
+                        </Typography>
+                        <Typography component="div" variant="h6" sx={{ textAlign: 'left' }}>
+                            E-Mail: Hans.juergen@mail.de
+                        </Typography>
+                    </CardContent>
+                </Box>
+                <CardMedia
+                    component="img"
+                    sx={{ width: 150 }}
+                    image={placeHolderImage}
+                    title="profileImage"/>
+            </Card>
         </div>
     );
 }
