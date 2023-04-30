bcdbacb7e93def449f78a047c6c163860a4c1e62
Elias Konson <ek103@hdm-stuttgart.de>
2023-04-30 22:48:15 +0200

    new id implementation

--- backend/src/server/bo/Blocklist.py
+++ backend/src/server/bo/Blocklist.py
@@ -1,19 +1,11 @@
 from BusinessObject import BusinessObject
 
 class Blocklist(BusinessObject):
-    blocklist_counter = 3000 # evtl den aktuellen Wert bei deployment immer aus der datenbank holen ?
     def __init__(self):
-        #super().__init__(id)
-        Blocklist.blocklist_counter+=1
-        self.__blocklist_id = Blocklist.blocklist_counter
+        super().__init__("Blocklist", [3001, 4000])
         self.__owner_id = 0
         self.__blocked_users = []
 
-    def get_blocklist_id(self):
-        return self.__blocklist_id
-
-    def set_blocklist_id(self, blocklist_id):
-        self.__blocklist_id = blocklist_id
 
     def get_owner_id(self):
         return self.__owner_id
@@ -28,12 +20,12 @@ class Blocklist(BusinessObject):
         self.__blocked_users = [users]
 
     def __str__(self):
-        return "Blocklist: {}, {}, {}".format(self.__blocklist_id, self.__owner_id, self.__blocked_users)
+        return "Blocklist: {}, {}, {}".format(self._id, self.__owner_id, self.__blocked_users)
 
     @staticmethod
     def from_dict(dictionary=dict()):
         obj = Blocklist()
-        obj.set_blocklist_id(dictionary["blocklist_id"])
+        obj.set_id(dictionary["id"])
         obj.set_owner_id(dictionary["owner_id"])
         obj.set_blocked_users(dictionary["blocked_users"])
         return obj
--- backend/src/server/bo/Bookmarklist.py
+++ backend/src/server/bo/Bookmarklist.py
@@ -1,19 +1,10 @@
 from BusinessObject import BusinessObject
 
 class Bookmarklist(BusinessObject):
-    bookmarklist_counter = 2000 # evtl den aktuellen Wert bei deployment immer aus der datenbank holen ?
     def __init__(self):
-        #super().__init__(id)
-        Bookmarklist.bookmarklist_counter+=1
-        self.__bookmarklist_id = Bookmarklist.bookmarklist_counter
+        super().__init__("Bookmarklist", [2001, 3000])
         self.__owner_id = 0
         self.__bookmarked_users = []
-
-    def get_bookmarklist_id(self):
-        return self.__bookmarklist_id
-
-    def set_bookmarklist_id(self, bookmarklist_id):
-        self.__bookmarklist_id = bookmarklist_id
 
     def get_owner_id(self):
         return self.__owner_id
@@ -28,12 +19,12 @@ class Bookmarklist(BusinessObject):
         self.__bookmarked_users = [users]
 
     def __str__(self):
-        return "Bookmarklist: {}, {}, {}".format(self.__bookmarklist_id, self.__owner_id, self.__bookmarked_users)
+        return "Bookmarklist: {}, {}, {}".format(self._id, self.__owner_id, self.__bookmarked_users)
 
     @staticmethod
     def from_dict(dictionary=dict()):
         obj = Bookmarklist()
-        obj.set_bookmarklist_id(dictionary["bookmarklist_id"])
+        obj.set_id(dictionary["id"])
         obj.set_owner_id(dictionary["owner_id"])
         obj.set_bookmarked_users(dictionary["bookmarked_users"])
         return obj
