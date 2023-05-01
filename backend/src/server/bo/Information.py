04fc1dd959b9b386346b7cb59a769b0d6fab59ab
Elias Konson <ek103@hdm-stuttgart.de>
2023-05-01 19:21:26 +0200

    first version of Information.py implementation

--- backend/src/server/bo/Information.py
+++ backend/src/server/bo/Information.py
@@ -1,8 +1,22 @@
-from server.bo import BusinessObject as bo
-from server.bo.Property import Property
+from BusinessObject import BusinessObject
 
-class Information(bo.BusinessObject):
+class Information(BusinessObject):
+    def __init__(self, value):
+        super().__init__("Information", [5001, 6000])
+        self.value = value
 
-    def __init__(self):
-        super().__init__()
-        self.value = Property
+    def set_value(self, value):
+        self.value = value
+
+    def get_value(self):
+        return self.value
+
+    def __str__(self):
+        return "Information: {}, {}".format(self._id, self.value)
+
+    @staticmethod
+    def from_dict(dictionary=dict()):
+        obj = Information()
+        obj.set_id(dictionary["id"])
+        obj.set_value(dictionary["value"])
+        return obj
