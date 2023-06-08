b4e5f48e77b347c881e99049a4b766b9858feda7
Michael Bergdolt <mb396@hdm-stuttgart.de>
2023-06-08 12:44:20 +0200

    minor changes to SimilarityMeasure.py

--- backend/src/server/bo/SimilarityMeasure.py
+++ backend/src/server/bo/SimilarityMeasure.py
@@ -1,26 +1,26 @@
 class SimilarityMeasure:
     def __init__(self, profile1, profile2):
         """initialize a new instance of SimilarityMeasure to compare two Profiles"""
-        self.__profile1 = profile1
-        self.__profile2 = profile2
+        self.__search_profile = profile1
+        self.__user_profile = profile2
 
     @staticmethod
-    def __get_num_sim(num1, num2):
+    def __get_num_sim(search_num, user_num):
         """Get the weighted similarity between two numbers
 
         The border is a threshold above which the similarity between two profiles is considered 0%
         The border is calculated by taking 10% of the higher number.
         After that the absolute amount of the difference of the two given numbers is calculated.
-        Then it is checked wether the difference is greater than the border and, if necessary, it gets replaced by the
+        Then it is checked whether the difference is greater than the border and, if necessary, it gets replaced by the
         border to avoid that the similarity value drops below 0.
         Finally, calculate the weighted deviation by dividing the result by the border Value and subtracting it from 1
         to convert the deviation into the similarity.
         """
         # calculate the border
-        border = max(num1, num2) / 10
+        border = search_num / 10
 
         # Absolute value of the difference
-        result = abs(num1 - num2)
+        result = abs(search_num - user_num)
 
         # If the result is greater than the border, set it to border value to obtain a maximum final return value of 1
         result = min(result, border)
@@ -30,7 +30,7 @@ class SimilarityMeasure:
         return 1 - result / border
 
     @staticmethod
-    def __get_string_sim(info1, info2):
+    def __get_string_sim(search_str, user_str):
         """Get the similarity between two strings
 
             The information string of the 2 profiles are split in the lowercase words of the two strings.
@@ -38,17 +38,17 @@ class SimilarityMeasure:
             Finally, the similar word count is divided threw the length of the shorter word list of phrases to get the
             similarity of the two phrases.
             """
-        info1 = info1.lower().split(" ")
-        info2 = info2.lower().split(" ")
+        search_str = search_str.lower().split(" ")
+        user_str = user_str.lower().split(" ")
         similar_words = 0
 
         # check for similaritys in the two lists
-        for word in info1:
-            if word in info2:
+        for word in search_str:
+            if word in user_str:
                 similar_words += 1
 
         # calculate and return the similarity of the two phrases
-        return similar_words / min(len(info1), len(info2))
+        return similar_words / min(len(search_str), len(user_str))
 
     def get_similarity_measure(self):
         """Get the similarity measure of two users
@@ -58,12 +58,12 @@ class SimilarityMeasure:
         """
         list_diff = []
 
-        for prop in self.__profile1:
-            if self.__profile1[prop] != "Keine Angabe" and self.__profile2[prop] != "Keine Angabe":
-                if (type(self.__profile1[prop])) == str:
-                    list_diff.append(self.__get_string_sim(self.__profile1[prop], self.__profile2[prop]))
+        for prop in self.__search_profile:
+            if self.__search_profile[prop] != "Keine Angabe" and self.__user_profile[prop] != "Keine Angabe":
+                if (type(self.__search_profile[prop])) == str:
+                    list_diff.append(self.__get_string_sim(self.__search_profile[prop], self.__user_profile[prop]))
                 else:
-                    list_diff.append(self.__get_num_sim(self.__profile1[prop], self.__profile2[prop]))
+                    list_diff.append(self.__get_num_sim(self.__search_profile[prop], self.__user_profile[prop]))
             # print(f"{i} Ähnlichkeitsmaß: {list_diff[-1]}")
 
         # If one User didn't set any Properties return 0 as similarity between the Users
