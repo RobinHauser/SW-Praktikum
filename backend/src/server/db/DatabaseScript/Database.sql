748a6b758ca9ce539aa6afb0516ba095ae87e070
Robin Hauser <rh086@hdm-stuttgart.de>
2023-06-21 18:29:05 +0200

    Added correct Database.sql

--- backend/src/server/db/DatabaseScript/Database.sql
+++ backend/src/server/db/DatabaseScript/Database.sql
@@ -105,10 +105,11 @@ create table chatrelation
         foreign key (UserID) references user (UserID)
 );
 
+create index chatrelation_chatcontainer_ChatID_fk
+    on chatrelation (ChatID);
+
 create index chatrelation_user_UserID_fk2
     on chatrelation (UserID);
-
-
 
 create table message
 (
@@ -127,13 +128,11 @@ create table chatcontainer
         primary key,
     ChatID         int not null,
     MessageID      int not null,
+    constraint chatcontainer_chatrelation_ChatID_fk
+        foreign key (ChatID) references chatrelation (ChatID),
     constraint fk_chatcontainer_message
         foreign key (MessageID) references message (MessageID)
 );
-
-create index chatcontainer_chatrelation_ChatID_fk
-    on chatcontainer (ChatID);
-
 
 create table profile_relation
 (
@@ -166,80 +165,67 @@ create table view
 );
 
 
-----------------------------------------------------------------------------------------------------
-# Befüllung
 -- user erstellen
-insert into user (UserID, Email, Displayname, AvatarURL) values (1001, 'ek103@hdm-stuttgart.de', 'Elias', 'tbd.de');
-insert into user (UserID, Email, Displayname, AvatarURL) values (1002, 'rh086@hdm-stuttgart.de', 'Robin', 'tbd.de');
-insert into user (UserID, Email, Displayname, AvatarURL) values (1003, 'bt036@hdm-stuttgart.de', 'Björn', 'tbd.de');
-insert into user (UserID, Email, Displayname, AvatarURL) values (1004, 'tk172@hdm-stuttgart.de', 'Theo', 'tbd.de');
-insert into user (UserID, Email, Displayname, AvatarURL) values (1005, 'mb396@hdm-stuttgart.de', 'Michael', 'tbd.de');
-insert into user (UserID, Email, Displayname, AvatarURL) values (1006, 'haug.jannik@gmail.com', 'Jannik', 'tbd.de');
-
--- blocklisten erstellen / zuweisen
-insert into blocklist (BlocklistID, UserID) VALUES (3001, 1001);
-insert into blocklist (BlocklistID, UserID) VALUES (3002, 1002);
-insert into blocklist (BlocklistID, UserID) VALUES (3003, 1003);
-insert into blocklist (BlocklistID, UserID) VALUES (3004, 1004);
-insert into blocklist (BlocklistID, UserID) VALUES (3005, 1005);
-insert into blocklist (BlocklistID, UserID) VALUES (3006, 1006);
-
--- bookmarklisten erstellen / zuweisen
-insert into bookmarklist (BookmarklistID, UserID) VALUES (2001, 1001);
-insert into bookmarklist (BookmarklistID, UserID) VALUES (2002, 1002);
-insert into bookmarklist (BookmarklistID, UserID) VALUES (2003, 1003);
-insert into bookmarklist (BookmarklistID, UserID) VALUES (2004, 1004);
-insert into bookmarklist (BookmarklistID, UserID) VALUES (2005, 1005);
-insert into bookmarklist (BookmarklistID, UserID) VALUES (2006, 1006);
-
--- blocks erzeugen
-insert into block (BlocklistID, BlockedUserID) VALUES (3002, 1004); -- robin blockt theo
-insert into block (BlocklistID, BlockedUserID) VALUES (3005, 1001); -- michi blockt elias
-
--- bookmarks erzeugen
-insert into bookmark (BookmarklistID, BookmarkedUserID) VALUES (2004, 1003); -- theo merkt björn
-insert into bookmark (BookmarklistID, BookmarkedUserID) VALUES (2006, 1005); -- jannik merkt michi
-
--- Messages befüllen
-INSERT INTO message (MessageID, Sender, Content, TimeStamp) VALUES (20001, 1004, 'Hallo', '2023-02-10 12:00:00');
-INSERT INTO message (MessageID, Sender, Content, TimeStamp) VALUES (20002, 1003, 'Heyy', '2023-02-10 12:00:00');
-INSERT INTO message (MessageID, Sender, Content, TimeStamp) VALUES (20003, 1004, 'Wie gehts dir', '2023-02-10 12:01:00');
-INSERT INTO message (MessageID, Sender, Content, TimeStamp) VALUES (20004, 1003, 'Gut soweit und dir?', '2023-02-10 12:01:00');
-INSERT INTO message (MessageID, Sender, Content, TimeStamp) VALUES (20005, 1004, 'Gut, danke der Nachfrage', '2023-02-10 12:05:00');
-INSERT INTO message (MessageID, Sender, Content, TimeStamp) VALUES (20006, 1004, 'Lass treffen?', '2023-02-10 12:10:00');
-INSERT INTO message (MessageID, Sender, Content, TimeStamp) VALUES (20007, 1003, 'WTF?', '2023-02-10 12:20:00');
-INSERT INTO message (MessageID, Sender, Content, TimeStamp) VALUES (20008, 1004, 'Ja so auf entspannt', '2023-02-10 12:40:00');
-INSERT INTO message (MessageID, Sender, Content, TimeStamp) VALUES (20009, 1003, 'Achso ja oke', '2023-02-10 12:50:00');
-INSERT INTO message (MessageID, Sender, Content, TimeStamp) VALUES (20010, 1003, '20Uhr kannst mich abholen', '2023-02-10 12:56:00');
-
--- Chatrelation befüllen
-INSERT INTO chatrelation (ChatID, UserID) VALUES (30001, 1004);
-INSERT INTO chatrelation (ChatID, UserID) VALUES (30001, 1003);
-
--- Chatcontainer befüllen
-INSERT INTO chatcontainer (ChatID, MessageID) VALUES (30001, 20001);
-INSERT INTO chatcontainer (ChatID, MessageID) VALUES (30001, 20002);
-INSERT INTO chatcontainer (ChatID, MessageID) VALUES (30001, 20003);
-INSERT INTO chatcontainer (ChatID, MessageID) VALUES (30001, 20004);
-INSERT INTO chatcontainer (ChatID, MessageID) VALUES (30001, 20005);
-INSERT INTO chatcontainer (ChatID, MessageID) VALUES (30001, 20006);
-INSERT INTO chatcontainer (ChatID, MessageID) VALUES (30001, 20007);
-INSERT INTO chatcontainer (ChatID, MessageID) VALUES (30001, 20008);
-INSERT INTO chatcontainer (ChatID, MessageID) VALUES (30001, 20009);
-INSERT INTO chatcontainer (ChatID, MessageID) VALUES (30001, 20010);
+insert into user (Email, Displayname, AvatarURL) values ('mitschiedielitschi@gmail.com', 'Mitschie Die Litschi', 'https://lh3.googleusercontent.com/a/AAcHTtedTZNO14zVppC5SEpk7Y0PqO1xQXHflen9WWCa=s96-c');
+insert into user (Email, Displayname, AvatarURL) values ('michaelbergdolt20@gmail.com', 'Michi B', 'https://lh3.googleusercontent.com/a/AGNmyxaXXuFmABo3wRfKm4RFHX-UXBgvXmMXoZFqVR956Q=s96-c');
+insert into user (Email, Displayname, AvatarURL) values ('haug.jannik@gmail.com', 'Jannik Haug', 'https://lh3.googleusercontent.com/a/AGNmyxZqDjGElyQDrT3qVr-8fC_l1e-JYfN72TKEKfUB=s96-c');
+insert into user (Email, Displayname, AvatarURL) values ('haug.jannik98@gmail.com', 'Jannik Haug', 'https://lh3.googleusercontent.com/a/AAcHTtfTsKPlHkmt1iujaHMUE6l8477mcIFpMKWPWrT3=s96-c');
+insert into user (Email, Displayname, AvatarURL) values ('sinnlos98@gmail.com', 'Frodo Beutlin', 'https://lh3.googleusercontent.com/a/AAcHTtc3uhg7g96OGDJwRDUwLG2WzTeqvRPVQZES3dY=s96-c');
+insert into user (Email, Displayname, AvatarURL) values ('bjoern.till@web.de', 'Björn', 'https://lh3.googleusercontent.com/a/AGNmyxYH3fcvoHQe0ZgAFC2nhHkjCxt6MRM1-wyMifU1wA=s96-c');
 
 
--- viewlist befüllen
-INSERT INTO viewedlist (ViewedListID, UserID) VALUES (15001, 1001);
-INSERT INTO viewedlist (ViewedListID, UserID) VALUES (15002, 1002);
-INSERT INTO viewedlist (ViewedListID, UserID) VALUES (15003, 1003);
-INSERT INTO viewedlist (ViewedListID, UserID) VALUES (15004, 1004);
-INSERT INTO viewedlist (ViewedListID, UserID) VALUES (15005, 1005);
-INSERT INTO viewedlist (ViewedListID, UserID) VALUES (15006, 1006);
+-- profile erstellen
+insert into profile (ProfileID, IsPersonal) VALUES (4001, 1);
+insert into profile (ProfileID, IsPersonal) VALUES (4002, 1);
+insert into profile (ProfileID, IsPersonal) VALUES (4003, 1);
+insert into profile (ProfileID, IsPersonal) VALUES (4004, 1);
+insert into profile (ProfileID, IsPersonal) VALUES (4005, 1);
+insert into profile (ProfileID, IsPersonal) VALUES (4006, 1);
+
+-- profile zuweisen
+insert into profile_relation (ProfileID, UserID) VALUES (4001, 1);
+insert into profile_relation (ProfileID, UserID) VALUES (4002, 2);
+insert into profile_relation (ProfileID, UserID) VALUES (4003, 3);
+insert into profile_relation (ProfileID, UserID) VALUES (4004, 4);
+insert into profile_relation (ProfileID, UserID) VALUES (4005, 5);
+insert into profile_relation (ProfileID, UserID) VALUES (4006, 6);
 
 
--- view befüllen
-INSERT INTO view (ViewID, ViewedListID, UserID) VALUES (16001, 15001, 1004);
-INSERT INTO view (ViewID, ViewedListID, UserID) VALUES (16002, 15004, 1002);
-INSERT INTO view (ViewID, ViewedListID, UserID) VALUES (16003, 15004, 1003);
+-- properties erstellen
+insert into property (PropertyID, Name, IsSelection, Description) VALUES (6001, 'Hair Color', 1, 'Please describe the color of your hair');
+insert into property (PropertyID, Name, IsSelection, Description) VALUES (6002, 'Height', 1, 'Please select your height');
+insert into property (PropertyID, Name, IsSelection, Description) VALUES (6003, 'Smoker', 1, 'Please state your smoking behaviour');
+insert into property (PropertyID, Name, IsSelection, Description) VALUES (6004, 'Religion', 1, 'Please state your religious beliefs');
+insert into property (PropertyID, Name, IsSelection, Description) VALUES (6005, 'First Name', 0, 'Please enter your first name');
+insert into property (PropertyID, Name, IsSelection, Description) VALUES (6006, 'Last Name', 0, 'Please enter your last name');
+insert into property (PropertyID, Name, IsSelection, Description) VALUES (6007, 'Biography', 0, 'Please describe yourself and your interests');
 
+
+-- property assignments erstellen
+insert into property_assignment (ValueID, PropertyID) VALUES (7001, 6001);
+insert into property_assignment (ValueID, PropertyID) VALUES (7002, 6001);
+insert into property_assignment (ValueID, PropertyID) VALUES (7003, 6001);
+insert into property_assignment (ValueID, PropertyID) VALUES (7004, 6002);
+insert into property_assignment (ValueID, PropertyID) VALUES (7005, 6002);
+insert into property_assignment (ValueID, PropertyID) VALUES (7006, 6003);
+insert into property_assignment (ValueID, PropertyID) VALUES (7007, 6003);
+insert into property_assignment (ValueID, PropertyID) VALUES (7008, 6007);
+insert into property_assignment (ValueID, PropertyID) VALUES (7009, 6007);
+
+-- Ausprägungen erstellen
+insert into occupancies (ValueID, Value) VALUES (7001, 'Brown');
+insert into occupancies (ValueID, Value) VALUES (7002, 'Schwarz');
+insert into occupancies (ValueID, Value) VALUES (7003, 'Blond');
+insert into occupancies (ValueID, Value) VALUES (7004, '< 2m');
+insert into occupancies (ValueID, Value) VALUES (7005, '> 2m');
+insert into occupancies (ValueID, Value) VALUES (7006, 'yes');
+insert into occupancies (ValueID, Value) VALUES (7007, 'no');
+insert into occupancies (ValueID, Value) VALUES (7008, 'Was geht ab');
+insert into occupancies (ValueID, Value) VALUES (7009, 'Dies ist meine Biographie');
+
+-- info objekte erzeugen
+insert into information (InformationID, ProfileID, ValueID) VALUES (5001, 4001, 7003);
+insert into information (InformationID, ProfileID, ValueID) VALUES (5002, 4004, 7006);
+insert into information (InformationID, ProfileID, ValueID) VALUES (5003, 4002, 7004);
+insert into information (InformationID, ProfileID, ValueID) VALUES (5004, 4001, 7005);
+insert into information (InformationID, ProfileID, ValueID) VALUES (5005, 4006, 7008);
