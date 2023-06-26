2e1e04eedfd32365d462a405e948ac037fdd2983
Michael Bergdolt <mb396@hdm-stuttgart.de>
2023-06-26 19:12:57 +0200

    changed BaseURL to local Backend

--- frontend/sopra-frontend/src/api/SopraDatingAPI.js
+++ frontend/sopra-frontend/src/api/SopraDatingAPI.js
@@ -16,8 +16,11 @@ export default class SopraDatingAPI {
     // Singleton instance
     static #api = null;
 
+    // deployed Python backend
+    // #SopraDatingServerBaseURL = 'https://sopra-projekt-390609.ey.r.appspot.com/';
+
     // Local Python backend
-    #SopraDatingServerBaseURL = 'https://sopra-projekt-390609.ey.r.appspot.com/';
+    #SopraDatingServerBaseURL = 'http://127.0.0.1:8000'
 
     // Local http-fake-backend
     //#SopraDatingServerBaseURL = 'http://localhost:8081/api/v1'
